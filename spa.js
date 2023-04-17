2b73c28faf39d19edb790c9b4bb53d775bb5f11b
Aleksandra Hinc <olka.hinc@gmail.com>
2023-04-17 12:23:26 +0200

    Single page application

---
+++ spa.js
@@ -0,0 +1,32 @@
+function changeContentToHome() {
+    var home1 = document.getElementById("home1");
+    var home2 = document.getElementById("home2");
+    var about = document.getElementById("about");
+    var contact = document.getElementById("contact");
+    home1.style.display = "block";
+    home2.style.display = "block";
+    about.style.display = "none";
+    contact.style.display = "none";
+}
+
+function changeContentToAbout() {
+    var home1 = document.getElementById("home1");
+    var home2 = document.getElementById("home2");
+    var about = document.getElementById("about");
+    var contact = document.getElementById("contact");
+    home1.style.display = "none";
+    home2.style.display = "none";
+    about.style.display = "block";
+    contact.style.display = "none";
+}
+
+function changeContentToContact() {
+    var home1 = document.getElementById("home1");
+    var home2 = document.getElementById("home2");
+    var about = document.getElementById("about");
+    var contact = document.getElementById("contact");
+    home1.style.display = "none";
+    home2.style.display = "none";
+    about.style.display = "none";
+    contact.style.display = "block";
+}
