f90258f0a16ec238e7e5d523741d6178e404f73a
Aleksandra Hinc <olka.hinc@gmail.com>
2023-06-18 23:30:19 +0200

    Changed the content to bears

--- script.js
+++ script.js
@@ -1,33 +1,33 @@
 function changeContentToHome() {
     var home1 = document.getElementById("home1");
     var home2 = document.getElementById("home2");
-    var about = document.getElementById("about");
+    var species = document.getElementById("species");
     var contact = document.getElementById("contact");
     home1.style.display = "block";
     home2.style.display = "block";
-    about.style.display = "none";
+    species.style.display = "none";
     contact.style.display = "none";
 }
 
-function changeContentToAbout() {
+function changeContentToSpecies() {
     var home1 = document.getElementById("home1");
     var home2 = document.getElementById("home2");
-    var about = document.getElementById("about");
+    var species = document.getElementById("species");
     var contact = document.getElementById("contact");
     home1.style.display = "none";
     home2.style.display = "none";
-    about.style.display = "block";
+    species.style.display = "block";
     contact.style.display = "none";
 }
 
 function changeContentToContact() {
     var home1 = document.getElementById("home1");
     var home2 = document.getElementById("home2");
-    var about = document.getElementById("about");
+    var species = document.getElementById("species");
     var contact = document.getElementById("contact");
     home1.style.display = "none";
     home2.style.display = "none";
-    about.style.display = "none";
+    species.style.display = "none";
     contact.style.display = "block";
 }
 
