04fb23437fd1f786b0460eef6eae307cf6db9ac8
Aleksandra Hinc <olka.hinc@gmail.com>
2023-06-19 02:06:29 +0200

    Species section layout

--- script.js
+++ script.js
@@ -1,32 +1,26 @@
 function changeContentToHome() {
-    var home1 = document.getElementById("home1");
-    var home2 = document.getElementById("home2");
+    var home = document.getElementById("home");
     var species = document.getElementById("species");
     var contact = document.getElementById("contact");
-    home1.style.display = "block";
-    home2.style.display = "block";
+    home.style.display = "block";
     species.style.display = "none";
     contact.style.display = "none";
 }
 
 function changeContentToSpecies() {
-    var home1 = document.getElementById("home1");
-    var home2 = document.getElementById("home2");
+    var home = document.getElementById("home");
     var species = document.getElementById("species");
     var contact = document.getElementById("contact");
-    home1.style.display = "none";
-    home2.style.display = "none";
+    home.style.display = "none";
     species.style.display = "block";
     contact.style.display = "none";
 }
 
 function changeContentToContact() {
-    var home1 = document.getElementById("home1");
-    var home2 = document.getElementById("home2");
+    var home = document.getElementById("home");
     var species = document.getElementById("species");
     var contact = document.getElementById("contact");
-    home1.style.display = "none";
-    home2.style.display = "none";
+    home.style.display = "none";
     species.style.display = "none";
     contact.style.display = "block";
 }
