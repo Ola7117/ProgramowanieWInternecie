5a87da8eb663d5ba2e6769cf26c7332239a221c6
Aleksandra Hinc <olka.hinc@gmail.com>
2023-06-19 11:46:59 +0200

    Added more text

--- language.js
+++ language.js
@@ -1,56 +1,56 @@
 const translations = {
     pl: {
         americanBlackBearHeader: "Niedźwiedź czarny",
-        americanBlackBearText: "Inne nazwy: baribal, niedźwiedź amerykański.",
+        americanBlackBearText: "<p>Inne nazwy: baribal, niedźwiedź amerykański.</p><p>Występowanie: Ameryka Północna.</p><p>Pomimo, że nazywany jest czarnym niedźwiedziem, jego sierść może być nawet płowa. Ma silny węch - siedem razy mocniejszy niż psa. Jego populacja przewyższa dwukrotnie wszystkie pozostałe gatunki niedźwiedzi razem wzięte.</p>",
         asianBlackBearHeader: "Niedźwiedź himlajaski",
-        asianBlackBearText: "Inne nazwy: niedźwiedź himalajski, niedźwiedź księżycowy, niedźwiedź obrożny.",
+        asianBlackBearText: "<p>Inne nazwy: niedźwiedź himalajski, niedźwiedź księżycowy, niedźwiedź obrożny.</p><p>Występowanie: południowa i wschodnia Azja.</p><p>Ma charakterystyczną białą plamę na piersi, w kształcie litery V lub obróconego półksiężyca. Posiada najsłabsze uzębienie ze wszystkich niedźwiedzi, więc przez większość roku żywi się pokarmem roślinnym. Wyjątkiem jest jesień, kiedy zbiera zapasy tłuszczu.</p>",
         brownBearHeader: "Niedźwiedź brunatny",
-        brownBearText: "Inne nazwy: -",
+        brownBearText: "<p>Inne nazwy: -</p><p>Występowanie: Ameryka Północna, Europa, północna oraz środkowa Azja.</p><p>Świetnie pływa. Populacja niedźwiedzi brunatnych w Ameryce Północnej nazywana jest niedźwiedziami grizzly, a te zamieszkujące archipelag Kodiaka - niedźwiedziami kodiackimi.</p>",
         contactNav: "Kontakt",
         contactStart: "Kontakt",
         contactText: "Jeśli masz jakąś sugestię co do strony, możesz wysłać mi wiadomość na adres: <a href='mailto:bearlyknowledgeable@example.com'>bearlyknowledgeable@example.com</a>.",
         cookiesText: "Ta strona używa plików cookie. Korzystanie z witryny bez zmiany ustawień Twojej przeglądarki oznacza, że będą one umieszczane w urządzeniu końcowym.",
         homeText: "Strona główna",
         pandaBearHeader: "Panda wielka",
-        pandaBearText: "Inne nazwy: niedźwiedź bambusowy.",
+        pandaBearText: "<p>Inne nazwy: niedźwiedź bambusowy.</p><p>Występowanie: Środkowe Chiny.</p><p>Panda ma specjalną kość nadagrstka nazywaną pseudo-kciukiem lub dodatkowym kciukiem. Jest to powiększona i wydłużona kość, która pozwala jej lepiej trzymać bambus.</p>",
         polarBearHeader: "Niedźwiedź polarny",
-        polarBearText: "Inne nazwy: niedźwiedź biały.",
+        polarBearText: "<p>Inne nazwy: niedźwiedź biały.</p><p>Występowanie: koło podbiegunowe północne.</p><p>Jest wysoce mięsożerny. Zaliczany jako ssak morski ze względu na dużą zależność od lodu, z krawędzi którego poluje. Jego futro jest tak naprawdę przezroczyste, a skóra czarna.</p>",
         slothBearHeader: "Wargacz leniwy",
-        slothBearText: "Inne nazwy: wargacz, aswal.",
+        slothBearText: "<p>Inne nazwy: wargacz, aswal.</p><p>Występowanie: Indie.</p><p>Zawdzięcza swoją nazwę długiej dolnej wardze i podniebieniu. Służą mu one do zjadania insektów.</p>",
         speciesNav: "Gatunki",
         speciesStart: "Gatunki",
         spectacledBearHeader: "Andoniedźwiedź okularowy",
-        spectacledBearText: "Inne nazwy: niedźwiedź andyski, niedźwiedź peruwiański, niedźwiedź okularowy.",
+        spectacledBearText: "<p>Inne nazwy: niedźwiedź andyski, niedźwiedź peruwiański, niedźwiedź okularowy.</p><p>Występowanie: Ameryka Południowa - Andy.</p><p>Nazwę zawdzięcza charakterystycznym plamom na pysku. Jest to jedyny niedźwiedź występujący w Ameryce Południowej. Mięso stanowi tylko 5-7% jego diety.</p>",
         sunBearHeader: "Biruang malajski",
-        sunBearText: "Inne nazwy: niedźwiedź malajski.",
+        sunBearText: "<p>Inne nazwy: niedźwiedź malajski.</p><p>Występowanie: Azja Południowo-Wschodnia.</p><p>Biruang to najmniejszy niedźwiedź, osiąga około 70 centymetrów wzrostu i od 25 do 65 kilogramów wagi. Jest też najbardziej nadrzewny, świetnie się wspina. Wygrzewa się na słońcu i śpi na wysokości 2-7 metrów nad ziemią.</p>",
         welcomeHeader: "Witaj na mojej stronie!",
         welcomeText: "Jest ona poświęcona niedźwiedziom. Niedźwiedziowate to rodzina ssaków zawierająca osiem występujących współcześnie gatunków - andoniedźwiedzia okularowego, biruanga malajskiego, niedźwiedzia brunatnego, niedźwiedzia czarnego, niedźwiedzia himalajskiego, niedźwiedzia polarnego, pandę wielką oraz wargacza leniwego. Znajdziesz tutaj ich zdjęcia oraz kilka informacji. Mam nadzieję, że dowiesz się czegoś ciekawego :)",
     },
 
     en: {
         americanBlackBearHeader: "American black bear",
-        americanBlackBearText: "Other names: black bear.",
+        americanBlackBearText: "<p>Other names: black bear.</p><p>Distribution: North America.</p><p>Despite being called the black bear, its fur can be even fawn. Has a very strong sense of smell - seven times better than the dog's. Its population is twice the size of all the other bear species combined.</p>",
         asianBlackBearHeader: "Asian black bear",
-        asianBlackBearText: "Other names: Asiatic black bear, moon bear, white-chested bear.",
+        asianBlackBearText: "<p>Other names: Asiatic black bear, moon bear, white-chested bear.</p><p>Distribution: South and East Asia.</p><p>It has a distinct white marking on the chest, shaped like a letter V or a rotated crescent moon. It has the weakest teeth among the bears, that is why it is mostly herbivorous through the bigger part of the year. Autumn is the exception, because then it stores fat.</p>",
         brownBearHeader: "Brown bear",
-        brownBearText: "Other names: -",
+        brownBearText: "<p>Other names: -</p><p>Distribution: North America, Europe, northern and central Asia.</p><p>It is great at swimming. The brown bears in North America are called grizzly bears, and on the Kodiak Islands - Kodiak bears.</p>",
         contactNav: "Contact",
         contactStart: "Contact",
         contactText: "If you have a suggestion regarding the website, you can send mail it to me: <a href='mailto:bearlyknowledgeable@example.com'>bearlyknowledgeable@example.com</a>.",
         cookiesText: "This website uses cookies. By staying on the website without changing your browser settings, you consent to the use of cookies on your device.",
         homeText: "Home",
         pandaBearHeader: "Giant panda",
-        pandaBearText: "Other names: panda bear, panda.",
+        pandaBearText: "<p>Other names: panda bear, panda.</p><p>Distribution: Central China.</p><p>Panda has a special wrist bone called a pseudo-thumb or an extra thumb. It's an enlarged and elongated bone that helps it grip bamboo better.</p>",
         polarBearHeader: "Polar bear",
-        polarBearText: "Inne nazwy: white bear.",
+        polarBearText: "<p>Other names: white bear.</p><p>Distribution: Artic Circle.</p><p>It is hypercarnivorous. Classified as a marine mammal, because it is very dependent on the sea ice, from the edge of which it hunts. Its fur is really transparent and the skin - black.</p>",
         slothBearHeader: "Sloth bear",
-        slothBearText: "Other names: Indian bear, labiated bear.",
+        slothBearText: "<p>Other names: Indian bear, labiated bear.</p><p>Distribution: India.</p><p>Its name comes from the long lower lip and palate. They allow it to suck up insects.</p>",
         speciesNav: "Species",
         speciesStart: "Species",
         spectacledBearHeader: "Spectacled Bear",
-        spectacledBearText: "Other names: South American bear, Andean bear, Andean short-faced bear, mountain bear.",
+        spectacledBearText: "<p>Other names: South American bear, Andean bear, Andean short-faced bear, mountain bear.</p><p>Distribution: South America - Andes mountains.</p><p>It gets its name from the characteristic facial markings. It is the only bear species native to South America. Its diet consists just in 5-7% of meat.</p>",
         sunBearHeader: "Sun bear",
-        sunBearText: "Other names: honey bear.",
+        sunBearText: "<p>Other names: honey bear.</p><p>Distribution: Southeast Asia.</p><p>Sun bear is the smallest among its family, it reaches around 70 centimeters in height and weighs between 25 and 65 kilograms. It is also the most arboreal of the bears, an exellent climber. It sunbathes and sleeps 2 to 7 meters above the ground.</p>",
         welcomeHeader: "Welcome to my website!",
         welcomeText: "It is dedicated to bears. Ursidae is a family of mammals which includes eight extant species - the spectacled bear, sun bear, brown bear, American black bear, Asian black bear, polar bear, giant panda and sloth bear. Here you will find their photos and some information about them. I hope that you will find out something interesting :)"
     }
@@ -91,28 +91,28 @@ document.addEventListener("DOMContentLoaded", function () {
 
 function changeLanguage(language) {
     americanBlackBearHeader.textContent = translations[language]["americanBlackBearHeader"];
-    americanBlackBearText.textContent = translations[language]["americanBlackBearText"];
+    americanBlackBearText.innerHTML = translations[language]["americanBlackBearText"];
     asianBlackBearHeader.textContent = translations[language]["asianBlackBearHeader"];
-    asianBlackBearText.textContent = translations[language]["asianBlackBearText"];
+    asianBlackBearText.innerHTML = translations[language]["asianBlackBearText"];
     brownBearHeader.textContent = translations[language]["brownBearHeader"];
-    brownBearText.textContent = translations[language]["brownBearText"];
+    brownBearText.innerHTML = translations[language]["brownBearText"];
     contactNav.textContent = translations[language]["contactNav"];
     contactStart.textContent = translations[language]["contactStart"];
     contactText.innerHTML = translations[language]["contactText"];
     cookiesText.innerHTML = translations[language]["cookiesText"];
     homeText.textContent = translations[language]["homeText"];
     pandaBearHeader.textContent = translations[language]["pandaBearHeader"];
-    pandaBearText.textContent = translations[language]["pandaBearText"];
+    pandaBearText.innerHTML = translations[language]["pandaBearText"];
     polarBearHeader.textContent = translations[language]["polarBearHeader"];
-    polarBearText.textContent = translations[language]["polarBearText"];
+    polarBearText.innerHTML = translations[language]["polarBearText"];
     slothBearHeader.textContent = translations[language]["slothBearHeader"];
-    slothBearText.textContent = translations[language]["slothBearText"];
+    slothBearText.innerHTML = translations[language]["slothBearText"];
     speciesNav.textContent = translations[language]["speciesNav"];
     speciesStart.textContent = translations[language]["speciesStart"];
     spectacledBearHeader.textContent = translations[language]["spectacledBearHeader"];
-    spectacledBearText.textContent = translations[language]["spectacledBearText"];
+    spectacledBearText.innerHTML = translations[language]["spectacledBearText"];
     sunBearHeader.textContent = translations[language]["sunBearHeader"];
-    sunBearText.textContent = translations[language]["sunBearText"];
+    sunBearText.innerHTML = translations[language]["sunBearText"];
     welcomeHeader.textContent = translations[language]["welcomeHeader"];
     welcomeText.textContent = translations[language]["welcomeText"];
 }
